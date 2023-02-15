bbc26da74c7472d185ae6bac77b1a388c6e8b1f7
Chen Gui <gui.g.chen@gmail.com>
2023-02-15 14:30:30 +0800

    Update web lissajous
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- web-lissajous/main.go
+++ web-lissajous/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+    "log"
     "image"
     "image/color"
     "image/gif"
     "io"
     "math"
     "math/rand"
+    "net/http"
     "os"
     "time"
+    "strconv"
 )
 
 var palette = []color.Color{
@@ -27,30 +30,52 @@ const (
     blueIndex  = 4
 )
 
+var (
+    cycles  = 5
+    size    = 100
+    nframes = 64
+    delay   = 8
+    res     = 0.001
+)
+
 func main() {
-    rand.Seed(time.Now().UTC().UnixNano())
-    lissajous(os.Stdout)
+    if false {
+        rand.Seed(time.Now().UTC().UnixNano())
+        lissajous(os.Stdout)
+    } else {
+        http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+            if err := r.ParseForm(); err != nil {
+                log.Print(err)
+            }
+            for k, v := range r.Form {
+                switch k {
+                case "cycles":
+                    cycles, _ = strconv.Atoi(v[0])
+                case "size":
+                    size, _ = strconv.Atoi(v[0])
+                case "nframes":
+                    nframes, _ = strconv.Atoi(v[0])
+                case "delay":
+                    delay, _ = strconv.Atoi(v[0])
+                }
+            }
+            lissajous(w)
+        })
+        log.Fatal(http.ListenAndServe("localhost:8000", nil))
+    }
 }
 
 func lissajous(out io.Writer) {
-    const (
-        cycles  = 5
-        res     = 0.001
-        size    = 100
-        nframes = 64
-        delay   = 8
-    )
-
     freq := rand.Float64() * 3.0
     anim := gif.GIF{LoopCount: nframes}
     phase := 0.0
     for i := 0; i < nframes; i++ {
         rect := image.Rect(0, 0, 2*size+1, 2*size+1)
         img := image.NewPaletted(rect, palette)
-        for t := 0.0; t < cycles*2*math.Pi; t += res {
+        for t := 0.0; t < float64(cycles)*2*math.Pi; t += res {
             x := math.Sin(t)
             y := math.Sin(t*freq + phase)
-            img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blueIndex)
+            img.SetColorIndex(size+int(x*float64(size)+0.5), size+int(y*float64(size)+0.5), blueIndex)
         }
         phase += 0.1
         anim.Delay = append(anim.Delay, delay)
