3a04c7a14d3ad38e839e9b355c9a9091deea64a5
Chen Gui <gui.g.chen@gmail.com>
2023-02-16 21:37:47 +0800

    Update github client
    Signed-off-by: Chen Gui <gui.g.chen@gmail.com>

--- go-github/github/github.go
+++ go-github/github/github.go
@@ -1,51 +1,159 @@
 package github
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/url"
-	"strings"
-	"time"
 )
 
-const IssueURL = "https://api.github.com/search/issues"
-
-type IssuesSearchResult struct {
-	TotalCount int `json:"total_count"`
-	Items      []*Issue
+type NewIssue struct {
+	Number int    `json:"number"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
 }
 
-type Issue struct {
-	Number    int
-	HTMLURL   string `json:"html_url"`
-	Title     string
-	State     string
-	User      *User
-	CreatedAt time.Time `json:"created_at"`
-	Body      string
+type SearchQuery struct {
+	Filter string `json:"filter"` // assigned,created,mentioned, subscribed, repos, all
+	State  string `json:"state"`  // open,closed,all
+	Sort   string `json:"sort"`   // created, updated, comments
+	Since  string `json:"since"`  // YYYY-MM-DDTHH:MM:SSZ
 }
 
-type User struct {
-	Login   string
-	HTMLURL string `json:"html_url"`
+const ApiURL = "https://api.github.com"
+const ApiToken = ""
+
+type Github struct {
+	IssueURL string
 }
 
-func SearchIssues(terms []string) (*IssuesSearchResult, error) {
-	q := url.QueryEscape(strings.Join(terms, " "))
-	resp, err := http.Get(IssueURL + "?q=" + q)
+func New(repo string) *Github {
+	return &Github{
+		IssueURL: fmt.Sprintf("%s/repos/%s/issues", ApiURL, repo),
+	}
+}
+
+func (g *Github) SetHeader(req *http.Request) {
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("Authorization", "Bearer "+ApiToken)
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+}
+
+func (g *Github) SearchIssues(query map[string]string) (*[]NewIssue, error) {
+	var param string
+	for k, v := range query {
+		if v != "" {
+			if param == "" {
+				param += "?" + k + "=" + v
+			} else {
+				param += "&" + k + "=" + v
+			}
+		}
+	}
+	fmt.Println(param)
+	req, err := http.NewRequest("GET", g.IssueURL+param, nil)
 	if err != nil {
 		return nil, err
 	}
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return nil, fmt.Errorf("search failed: %s", resp.Status)
-	}
-	var result IssuesSearchResult
-	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
-		resp.Body.Close()
+	g.SetHeader(req)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
 		return nil, err
 	}
-	resp.Body.Close()
-	return &result, nil
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("get failed: %s", resp.Status)
+	}
+	issues := []NewIssue{}
+	if err := json.NewDecoder(resp.Body).Decode(&issues); err != nil {
+		return nil, err
+	}
+	return &issues, nil
+}
+
+func (g *Github) CreateIssue(issue *NewIssue) (bool, error) {
+	body := &bytes.Buffer{}
+	if err := json.NewEncoder(body).Encode(issue); err != nil {
+		return false, err
+	}
+	req, err := http.NewRequest("POST", g.IssueURL, body)
+	if err != nil {
+		return false, err
+	}
+	g.SetHeader(req)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return false, fmt.Errorf("get failed: %s", resp.Status)
+	}
+	return true, nil
+}
+
+func (g *Github) GetIssue(id int) (*NewIssue, error) {
+	issueURL := fmt.Sprintf("%s/%d", g.IssueURL, id)
+	req, err := http.NewRequest("GET", issueURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	g.SetHeader(req)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("get failed: %s", resp.Status)
+	}
+	var issue NewIssue
+	if err := json.NewDecoder(resp.Body).Decode(&issue); err != nil {
+		return nil, err
+	}
+	return &issue, nil
+}
+
+func (g *Github) UpdateIssue(id int, issue *NewIssue) (bool, error) {
+	body, err := json.Marshal(issue)
+	if err != nil {
+		return false, err
+	}
+	issueURL := fmt.Sprintf("%s/%d", g.IssueURL, id)
+	req, err := http.NewRequest("PATCH", issueURL, bytes.NewReader(body))
+	if err != nil {
+		return false, err
+	}
+	g.SetHeader(req)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return false, fmt.Errorf("get failed: %s", resp.Status)
+	}
+	return true, nil
+}
+
+func (g *Github) CloseIssue(id int) (bool, error) {
+	body, err := json.Marshal(map[string]string{"state": "closed"})
+	if err != nil {
+		return false, err
+	}
+	issueURL := fmt.Sprintf("%s/%d", g.IssueURL, id)
+	req, err := http.NewRequest("PATCH", issueURL, bytes.NewReader(body))
+	if err != nil {
+		return false, err
+	}
+	g.SetHeader(req)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return false, fmt.Errorf("get failed: %s", resp.Status)
+	}
+	return true, nil
 }
--- go-github/main.go
+++ go-github/main.go
@@ -1,19 +1,92 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"go-github/github"
-	"log"
 	"os"
 )
 
 func main() {
-	result, err := github.SearchIssues(os.Args[1:])
-	if err != nil {
-		log.Fatal(err)
+	gh := github.New("chengui/zypp-garden")
+
+	searchCmd := flag.NewFlagSet("search", flag.ExitOnError)
+	searchFilter := searchCmd.String("filter", "", "search filter")
+	searchState := searchCmd.String("state", "", "search state")
+	searchSort := searchCmd.String("sort", "", "search sort")
+
+	createCmd := flag.NewFlagSet("create", flag.ExitOnError)
+	createTitle := createCmd.String("title", "", "issue title")
+	createBody := createCmd.String("body", "", "issue body")
+
+	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+	getId := getCmd.Int("id", 0, "issue id")
+
+	updateCmd := flag.NewFlagSet("update", flag.ExitOnError)
+	updateId := updateCmd.Int("id", 0, "issue id")
+	updateTitle := updateCmd.String("title", "", "issue title")
+	updateBody := updateCmd.String("body", "", "issue body")
+
+	closeCmd := flag.NewFlagSet("close", flag.ExitOnError)
+	closeId := closeCmd.Int("id", 0, "issue id")
+
+	if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(1)
 	}
-	fmt.Printf("%d issues:\n", result.TotalCount)
-	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n", item.Number, item.User.Login, item.Title)
+
+	switch os.Args[1] {
+	case "search":
+		searchCmd.Parse(os.Args[2:])
+		issues, err := gh.SearchIssues(map[string]string{
+			"filter": *searchFilter,
+			"state":  *searchState,
+			"sort":   *searchSort,
+		})
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(2)
+		}
+		fmt.Println(issues)
+	case "create":
+		createCmd.Parse(os.Args[2:])
+		_, err := gh.CreateIssue(&github.NewIssue{
+			Title: *createTitle,
+			Body:  *createBody,
+		})
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(2)
+		}
+		fmt.Println("create successfully")
+	case "get":
+		getCmd.Parse(os.Args[2:])
+		issue, err := gh.GetIssue(*getId)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(2)
+		}
+		fmt.Println(issue)
+	case "update":
+		updateCmd.Parse(os.Args[2:])
+		_, err := gh.UpdateIssue(*updateId, &github.NewIssue{
+			Title: *updateTitle,
+			Body:  *updateBody,
+		})
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(2)
+		}
+		fmt.Printf("update %v successfully.\n", updateId)
+	case "close":
+		closeCmd.Parse(os.Args[2:])
+		_, err := gh.CloseIssue(*closeId)
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			os.Exit(2)
+		}
+		fmt.Printf("close %v successfully.\n", closeId)
+	default:
+		flag.Usage()
 	}
 }
